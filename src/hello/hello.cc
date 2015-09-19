b06be6551e8539d079e634781926836477f7fa66
ymyang <ymyang@163.com>
2015-09-19 15:57:53 +0800

    hello

--- src/hello/hello.cc
+++ src/hello/hello.cc
@@ -3,23 +3,23 @@
 
 namespace demo {
 
-    using v8::FunctionCallbackInfo;
-    using v8::HandleScope;
-    using v8::Isolate;
-    using v8::Local;
-    using v8::Object;
-    using v8::String;
-    using v8::Value;
+  using v8::FunctionCallbackInfo;
+  using v8::HandleScope;
+  using v8::Isolate;
+  using v8::Local;
+  using v8::Object;
+  using v8::String;
+  using v8::Value;
 
-    void Method(const FunctionCallbackInfo<Void>& args) {
-        Isolate* isolate = args.GetIsolate();
-        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
-    }
+  void Method(const FunctionCallbackInfo<Value>& args) {
+     Isolate* isolate = args.GetIsolate();
+     args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  }
 
-    void Init(Local<Object> exports) {
-        NODE_SET_METHOD(exports, "hello", Method);
-    }
+  void init(Local<Object> exports) {
+     NODE_SET_METHOD(exports, "hello", Method);
+  }
 
-    NODE_MODULE(addon, Init)
+  NODE_MODULE(addon, init)
 
-}
+}  // namespace demo
--- src/hello/hello.js
+++ src/hello/hello.js
@@ -1,5 +1,5 @@
 /**
  * Created by yang on 2015/9/19.
  */
-var hello = require('.build/Release/hello');
+var hello = require('../../build/Release/hello');
 console.log(hello.hello());
